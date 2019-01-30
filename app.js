56272cf96629148caf068c6bd03683e3ed2727be
Venkateswararao Lankalapally <vlankala@callidussoftware.com>
2019-01-30 14:59:05 +0530

    added dist as a static folder in app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}) );
 
-app.use( express.static(__dirname + '/client' ) );
+app.use( express.static(__dirname + '/dist/angular-firebase-auth' ) );
 
 var listener = server.listen(process.env.PORT || 5000, function(){
     console.log('Listening on port ' + listener.address().port); //Listening on port 5000
